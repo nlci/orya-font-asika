9eaa04f9e775be96b9aa21ec48c5a1d7637b5ba5
Bobby de Vos <bobby_devos@sil.org>
2019-01-02 16:52:40 -0700

    Refactor adding characters in bulk

--- addchars.py
+++ addchars.py
@@ -1,63 +1,26 @@
 #!/bin/python
 
-import os
-import os.path
-import sys
-
-from wscript import *
-
-charis_dir = '../../../latn/fonts/charis_local/5.000/zip/unhinted/'
-charis_ttf = '/CharisSIL'
-gentium_dir = '../../../latn/fonts/gentium_local/basic/1.102/zip/unhinted/'
-gentium_ttf = '/GenBkBas'
-sophia_dir = '../../../copt/fonts/sophia_nubian/1.000/zip/unhinted/'
-sophia_ttf = '/SN'
-annapurna_dir = '../../../deva/fonts/annapurna_local/1.203/zip/unhinted/'
-annapurna_ttf = '/AnnapurnaSIL-'
-panini = '../../../deva/fonts/panini-master/source/Panini'
-deva = '../../../deva/fonts/panini/source/'
-thiruvalluvar = '../../../taml/fonts/thiruvalluvar/source/ThiruValluvar'
-vaigai = '../../../taml/fonts/thiruvalluvar/source/Vaigai'
-exo = '../../../latn/fonts/exo/1.500/zip/unhinted/1000/Exo-'
-
-def runCommand(cmd, ifont, ofont):
-    cmd = 'ffcopyglyphs' + ' -f ' + cmd + ' ' + ifont + ' ' + ofont
-    print cmd
-    os.system(cmd)
-
-def findFile(filename):
-    return os.path.join(sys.argv[1], filename)
-
-def modifyFile(cmd, filename):
-    tmp = 'tmp.sfd'
-    os.rename(findFile(filename), tmp)
-    runCommand(cmd, tmp, findFile(filename))
-    os.remove(tmp)
+from addcharslib import *
 
 def modifySource(sfd, f, s, sn):
     print sfd
 
-    emsize = '1000'
-    emext = '.sfd'
-    emopt = '-s ' + str(1000.0/2048.0/1.4) + ' '
-    devaf = 'Maurya'
-
-    cmd = '-i ' + vaigai + '-' + sn + '.sfd' + ' --rangefile cs/thiruvalluvar/main.txt'
-    modifyFile(cmd, sfd)
-
-    cmd = '-i ' + deva + devaf + '-' + sn + '.sfd' + ' --rangefile cs/panini/main4orya.txt'
-    modifyFile(cmd, sfd)
+    workshop = 1.4
+    upm2048 = 1000.0/2048.0
+    upm1000 = 1.0
+    scale2048 = '-s ' + str(upm2048/workshop) + ' '
+    scale1000 = '-s ' + str(upm1000/workshop) + ' '
 
     asn = sn
     asn = asn.replace('BoldItalic', 'Bold')
     asn = asn.replace('Italic', 'Regular')
-    cmd = '-s ' + str(1/1.4) + ' ' + '-i ' + annapurna_dir + emsize + annapurna_ttf + asn + emext + ' --rangefile cs/annapurna/main.txt'
+    cmd = scale2048 + '-i ' + annapurna + asn + '.ttf' + ' --rangefile cs/annapurna/main.txt'
     modifyFile(cmd, sfd)
 
     sns = s.replace('-', '')
-    cmd = '-s ' + str(1/1.4) + ' ' + '-i ' + sophia_dir + '1000' + sophia_ttf + sns + '.ttf' + ' --rangefile cs/sophia/main.txt'
+    cmd = scale1000 + '-i ' + sophia + sns + '.ttf' + ' --rangefile cs/sophia/main.txt'
     modifyFile(cmd, sfd)
-    cmd = emopt + '-i ' + charis_dir + '2048' + charis_ttf + s + '.ttf' + ' --rangefile cs/charis/composite4sophia.txt --rangefile cs/charis/extra4sophia.txt'
+    cmd = scale2048 + '-i ' + charis + s + '.ttf' + ' --rangefile cs/charis/composite4sophia.txt --rangefile cs/charis/extra4sophia.txt'
     modifyFile(cmd, sfd)
 
 for f in faces:
